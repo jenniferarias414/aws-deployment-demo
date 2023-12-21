8d27a4f96e5c31caac9995da55f1a5531afc5476
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2023-12-21 14:49:05 -0600

    added rollbar errors

--- public/main.js
+++ public/main.js
@@ -1,5 +1,54 @@
-const btn = document.querySelector('button')
+const btn = document.getElementById('getName')
 
-const clickHandler = () => alert('The cat name is: Rascal')
+const clickHandler = () => alert('The cat name is: Lord Whiskerfluff Gigglepaws')
 
 btn.addEventListener('click', clickHandler)
+
+const addForm = document.querySelector('form');
+const nameInput = document.querySelector('input');
+const container = document.querySelector('section');
+
+function putTheThingInTheView(res) {
+    container.innerHTML = ''
+    nameInput.value = ''
+
+    res.data.forEach((catName, index) => {
+        container.innerHTML += `<p name=${index}>${catName}</p>`
+    })
+
+    document.querySelectorAll('p').forEach(element => {
+        const theIndexValue = element.getAttribute('name');
+
+        element.addEventListener('click', () => {
+            axios
+                .delete(`/api/cats/${theIndexValue}`)
+                .then(res => {
+                    putTheThingInTheView(res)
+                })
+        })
+    })
+}
+
+function submitHandler(evt) {
+    evt.preventDefault();
+
+    axios
+        .post('/api/cats', { name: nameInput.value })
+        .then(res => {
+            putTheThingInTheView(res)
+        })
+        .catch(err => {
+            nameInput.value = ''
+
+            const notif = document.createElement('aside')
+            notif.innerHTML = `<p>${err.response.data}</p>
+            <button class="close">close</button>`
+            document.body.appendChild(notif)
+
+            document.querySelectorAll('.close').forEach(btn => {
+                btn.addEventListener('click', (e) => {
+                    e.target.parentNode.remove()
+                })
+            })
+        })
+}
--- server.js
+++ server.js
@@ -14,6 +14,8 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
+const cats = []
+
 app.get('/', (req, res) => {
     try {
         nonExistentFunction();
@@ -23,5 +25,29 @@ app.get('/', (req, res) => {
         // (Note: the exact output may be browser-dependent)
       }
 })
+
+app.post('/api/cats', (req, res) => {
+    let {name} = req.body
+     rollbar.critical(`New kitty added to the list of shame! Welcome: ${name}`)
+    const index = cats.findIndex(cat => {
+        return cat === name
+    })
+ 
+    try {
+        if (index === -1 && name !== '') {
+            cats.push(name)
+            res.status(200).send(cats)
+        } else if (name === ''){
+            rollbar.warning("dude. please follow directions. type a name!")
+            res.status(400).send('You must enter a fluffy kitty name.')
+        } else {
+            rollbar.warning("Woah! Don't duplicate my kitties, please.")
+            res.status(400).send('That kitty already exists.')
+        }
+    } catch (err) {
+        console.log(err)
+        rollbar.error("Bloop: error detected...")
+    }
+ })
 
 app.listen(4000, () => console.log('up on 4000'))
