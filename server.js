d209a6a5151ab454724a9cca7a33a49e62c11542
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2023-12-21 14:14:52 -0600

    added tracibility error

--- server.js
+++ server.js
@@ -3,4 +3,25 @@ const app = express()
 
 app.use(express.static(`${__dirname}/public`))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'f234b52a68eb49368d2893f2c7795bc7',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+app.get('/', (req, res) => {
+    try {
+        nonExistentFunction();
+      } catch (error) {
+        console.error(error);
+        // Expected output: ReferenceError: nonExistentFunction is not defined
+        // (Note: the exact output may be browser-dependent)
+      }
+})
+
 app.listen(4000, () => console.log('up on 4000'))
